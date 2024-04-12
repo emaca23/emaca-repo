5c97a01fab248d75926e7ec60a55c0b00932563b
Ed Charleson <edward.charleson@gmail.com>
2024-04-12 16:03:46 +0000

    echarleson4.12.2024

---
+++ project1.Python.py
@@ -0,0 +1,18 @@
+#Scenario: Your company needs to learn about the files located on various machines. 
+#You have been asked to build a script that extracts information such as the 'name' and
+#'size' about the files in the current working directory and stores it in a list of dictionaries.
+
+#Create a script that generates a list of dictionaries about files in the working directory. 
+#Then print the list.
+#Push your code to GitHub and include the link in your write up
+#size {}
+#working_directory = {}
+#print(len(numbers))
+#print(type(numbers), number)
+
+file = {}
+file ['dictionaries.py'] = '600kps'
+file ['Hello_World.py'] = '108kps'
+print(file)
+print(len(file))
+
