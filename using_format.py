04a8ce354d7d52cff66bd99f0b0f6c5a0fc5a868
Ed Charleson <edward.charleson@gmail.com>
2024-04-23 01:06:54 +0000

    echarleson4222024

---
+++ using_format.py
@@ -0,0 +1,12 @@
+greeting = 'Hello'
+name = 'Michael'
+
+#message = '{}, {}. Welcome!'.format(greeting, name)
+
+#message = f'{greeting}, {name}. Welcome!'
+
+message = f'{greeting}, {name.upper()}. Welcome!'
+print (message)
+
+#
+#
