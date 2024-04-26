a896679b48fe7108078f3b0dc94f9252628ede77
Ed Charleson <edward.charleson@gmail.com>
2024-04-26 16:36:31 +0000

    echarleson04262024

---
+++ Lists_and_Tuples4.26.2024.py
@@ -0,0 +1,82 @@
+courses = ['history', 'math', 'physics', 'science']
+
+course_str = ' , '.join(courses)
+
+new_list = course_str.split('  -  ')
+
+#print(course_str)
+
+print(new_list)
+
+
+#nums = [1,2,3,4,5]
+
+#print(courses.index('art'))
+
+#print('math' in courses)
+
+#for course in courses:  #for loop
+#    print(course)
+
+#for index, course in enumerate(courses, start=1):  #for loop
+    #print(index, course)
+
+
+
+
+#sorted_courses = sorted(courses) # sorted- function- allows you to sort a list without altering the original version by creating a new 
+#variable, in this case sorted_courses
+
+#courses.sort(reverse=True)
+
+#nums.sort(reverse=True)
+
+#print(min(nums))
+
+#print(max(nums))
+
+#print (sum(nums))
+
+#print(courses)
+
+# print(sorted_courses)
+
+#courses.append('art')
+
+#courses.insert(0,'art')
+
+#courses.remove('math')
+
+#courses.pop()
+
+#popped= courses.pop()
+#print(popped)
+
+#courses_2 = ['art', 'education']
+
+# courses.insert(0, courses_2)
+
+#courses.extend(courses_2)
+
+# print(courses)
+
+# courses.reverse()
+
+# courses.sort()
+# print(courses)
+
+#print(courses[0])
+
+#print(len(courses))
+
+#print(courses[0])
+
+#print(courses[3])
+
+#print(courses [-1]) # -1 is always the last item
+
+#print(courses[0:2]) # second index is not inclusive, does not include 2
+
+#print(courses[2:]) # start at first index and goes to the end, can do the same in reverse
+
+#courses.append('art')
